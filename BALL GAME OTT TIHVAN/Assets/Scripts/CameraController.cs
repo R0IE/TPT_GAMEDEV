322e8f714d533ccb73a5514621e2205bbcdcbcff
Ott Tihvan <otttihvan@gmail.com>
2024-10-16 11:33:07 +0300

    Update
    Update

--- BALL GAME OTT TIHVAN/Assets/Scripts/CameraController.cs
+++ BALL GAME OTT TIHVAN/Assets/Scripts/CameraController.cs
@@ -17,6 +17,8 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + 5, Player.transform.position.z - 10);
+        Camera mycam = GetComponent<Camera>();
+Vector3 target = mycam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
+transform.LookAt(target, Vector3.up);
     }
 }
